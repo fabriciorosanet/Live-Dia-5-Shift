2467877920a2bf61dcfbac68c27bd038b56d5707
Fabricio Rosa <fabriciorosafiap@gmail.com>
2024-07-26 11:54:09 -0300

    Aula de loops: While, For,  Do While

--- Program.cs
+++ Program.cs
@@ -14,3 +14,35 @@ while (i <= 50) // repete o conteúdo do bloco enquanto i <= 10
 
 */
 
+/* 
+ * LOOP FOR
+ 
+int num, resp;
+Console.Write("Digite um número para ver a tabuada: ");
+num = Convert.ToInt32(Console.ReadLine());
+
+for (int i = 1; i <= 50; i++) // no for, o bloco é iniciado com as definições de inicio, fim e incremento do contador
+{
+    resp = num * i;
+    Console.WriteLine("{0} x {1} = {2}", num.ToString(), i.ToString(), resp.ToString());
+}
+
+*/
+
+/*
+ * LOOP DO WHILE
+
+
+int num, resp, i;
+Console.Write("Digite um número para ver a tabuada: ");
+num = Convert.ToInt32(Console.ReadLine());
+i = 1; // inicio do contador
+
+do // inicio do bloco de repetição
+{
+    resp = num * i;
+    Console.WriteLine("{0} x {1} = {2}", num.ToString(), i.ToString(), resp.ToString());
+    i++; //incremento do contador
+}while (i <= 50);
+
+ */
